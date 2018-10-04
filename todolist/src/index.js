45f30fd24a79d71798de7c904c093d5a18a88d6b
Frances Yu <francesyu90@yahoo.com>
2018-10-04 09:17:16 -0700

    feat: Added code for creating store and passing store down

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -3,5 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducer from './reducers';
+import middleware from './middleware';
+
+const store = createStore(reducer, middleware);
+
+ReactDOM.render(
+    <Provider>
+        <App store={store} />
+    </Provider>, document.getElementById('root'));
 
